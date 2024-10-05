af81a5efcd7817d76d2612143ddb765696603fdf
nchis003 <[nchis003@odu.edu]>
2024-10-05 12:38:05 -0400

    working list!!

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -2,51 +2,44 @@ import React from 'react';
 import '../css/patientList.css';
 
 import { BsFillPencilFill, BsFillTrashFill } from "react-icons/bs";
-export default function PatientList () {
+
+
+export const PatientList = ({ rows, deletePatient, editRow }) => {
     return (
     <div className='PatientTable'>
         <table className='Ptable'>
-        <thead>
-            <tr>
-                <th className='expand'>Name</th>
-                <th>Phone Number</th>
-                <th>Status</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
+            <thead>
+                <tr>
+                    <th className='expand'>Name</th>
+                    <th>Phone Number</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
 
-        <tbody>
-            <tr>
-                <td> Jane Doe </td>
-                <td> 757 123 4567</td>
-                <td> 
-                    <span className='label label-active'>Active</span>
-                </td>
-                <td>
-                    <span>
-                        <BsFillTrashFill className='delete-btn'/>
-                        <BsFillPencilFill/>
+            <tbody>
+                {rows.map((row, idx) => {
+                    const statusText = row.status.charAt(0).toUpperCase() + row.status.slice(1);
+                    return (
+                        <tr key={idx}>
+                            <td>{row.name}</td>
+                            <td className='pnumber'>{row.number}</td>
+                            <td> 
+                                <span className={`label label-${row.status}`}>
+                                    {statusText}
+                                </span>
+                            </td>
+                            <td>
+                                <span className='actions'>
+                                    <BsFillTrashFill className='delete-btn' onClick={() => deletePatient(idx)}/>
+                                    <BsFillPencilFill onClick={() => editRow(idx)}/>
+                                </span>
+                            </td>
+                        </tr>
+                    );
+                })}
 
-                    </span>
-                </td>
-            </tr>
-
-            <tr>
-                <td> John Doe </td>
-                <td> 757 123 0000</td>
-                <td> 
-                    <span className='label label-inactive'>Inactive</span>
-                </td>
-                <td>
-                    <span>
-                        <BsFillTrashFill className='delete-btn'/>
-                        <BsFillPencilFill />
-
-                    </span>
-                </td>
-            </tr>
-
-        </tbody>
+            </tbody>
 
         </table>
     </div>
--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,21 +1,61 @@
 import React, { useState } from 'react';
 import { Modal } from '../components/modal';
+import { PatientList } from '../components/patientList';
 import '../css/modal.css';
 import '../css/patientList.css';
-import PatientList from '../patientComponents/PatientList';
 
 
 export default function Patients() {
     const [modalOpen, setModalOpen] = useState(false)
+
+    //Storafe for patient data
+    const [rows, setRows] = useState([ //can have empty for demonstartion. just sample data for testing
+        {name: "Jane Doe", number: "000-000-0000", status:"active"}, 
+        {name: "John Adams", number: "111-111-1111", status:"active"}
+    ]);
+
+    const [rowToEdit, setRowToEdit] = useState(null);
+
+    const deleteRow = (targetIndex) => {
+        setRows(rows.filter((_, idx) => idx !== targetIndex));
+    };
+
+    const handleEditRow = (idx) => {
+        setRowToEdit(idx);
+
+        setModalOpen(true);
+    } 
+
+    const handleSubmit = (newRow) => {
+        rowToEdit === null ?
+        setRows([...rows, newRow]) 
+        : setRows(
+            rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+            })
+        );
+    };
+
     return (
 
         <>
             <h1>Patients</h1>
             <a href="/patient_dashboard">to patient dash</a>
 
-            <PatientList />
+            <PatientList rows={rows} deletePatient={deleteRow} editRow={handleEditRow}/>
             <button className='btn' onClick={() => setModalOpen(true)}> Add Patient</button>
-            {modalOpen && (<Modal closeModal={()=> {setModalOpen(false);}} /> )}
+            {modalOpen && (
+                <Modal 
+                    closeModal={()=> {
+                        setModalOpen(false);
+                        setRowToEdit(null);
+                    }} 
+                    onSubmit={handleSubmit}
+                    defaultValue={rowToEdit != null && rows[rowToEdit]}
+                /> 
+            )}
 
 
 
