803db42d0bdadd428729c0b3865c3fbd58534b75
Gabriella Marotta <missymiss1223@gmail.com>
2024-12-03 15:03:39 -0500

    update

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -55,7 +55,7 @@ export default function Patients() {
                 }
             );
 
-            setRows(response?.data)
+            getPatients()
         }
         catch(err) {
             console.log(err)
